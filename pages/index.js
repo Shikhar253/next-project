813122e9708237aac8de170d3bea35cc4f8869f4
Shikhar253 <saxenashikhar01@gmail.com>
2022-06-10 11:51:35 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,21 @@ import Features from "../components/Features";
 import CTA from "../components/CTA";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+
+const queryClient = new QueryClient();
+
 export default function Home() {
   return (
-    <Layout>
-      <Head>
-        <title>TIF TASK</title>
-      </Head>
-      <Header />
-      <Features />
-      <CTA />
-      <Footer />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Head>
+          <title>TIF TASK</title>
+        </Head>
+        <Header />
+        <Features />
+        <CTA />
+        <Footer />
+      </Layout>
+    </QueryClientProvider>
   );
 }
